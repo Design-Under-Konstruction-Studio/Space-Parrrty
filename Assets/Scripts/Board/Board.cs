a5c93edd5fd3ab41e8c380a95b8a42b5ad2a4d08
Jordan Oliveira <jordandossantosoliveira@gmail.com>
2019-11-04 23:59:58 -0300

    WIP: Function CreateBoard partials finish #5

--- Assets/Scripts/Board/Board.cs
+++ Assets/Scripts/Board/Board.cs
@@ -1,41 +1,62 @@
-﻿using System.Collections;
-using System.Collections.Generic;
+﻿using System.Collections.Generic;
 using UnityEngine;
 
-public class Board : MonoBehaviour {
-  [Header ("Prefabs Imports")]
-  [SerializeField]
-  private List<GameObject> _tilesPrefabs;
+namespace BoardController
+{
+  public class Board : MonoBehaviour
+  {
+    [Header("Prefabs Imports")]
+    [SerializeField]
+    private List<GameObject> _tilesPrefabs;
 
-  [Header ("Board Settings")]
-  [SerializeField]
-  private Vector2Int _boardSize;
+    [Header("Board Settings")]
+    [SerializeField]
+    private Vector2Int _boardSize;
 
-  private GameObject[, ] _boardTiles;
+    private GameObject[,] _boardTiles;
 
-  // Start is called before the first frame update
-  void Start () {
-    CreateBoard ();
-  }
+    // Start is called before the first frame update
+    void Start()
+    {
+      CreateBoard();
+    }
 
-  // Update is called once per frame
-  void Update () {
+    // Update is called once per frame
+    void Update()
+    {
 
-  }
+    }
 
-  void CreateBoard () {
-    _boardTiles = new GameObject[_boardSize.x, _boardSize.y];
+    void CreateBoard()
+    {
+      _boardTiles = new GameObject[_boardSize.x, _boardSize.y];
 
-    GameObject tilesObs = new GameObject ();
-    tilesObs.name = "tilesObs";
-    tilesObs.transform.SetParent (this.transform);
-    for (int y = 0; y < _boardSize.y; y++) {
-      for (int x = 0; x < _boardSize.x; x++) {
-        int randomNumber = Random.Range (0, 4);
-        GameObject tile = Instantiate (_tilesPrefabs[randomNumber], tilesObs.transform);
-        tile.name = $"Tile {x}-{y}";
-        tile.transform.position = new Vector2 (x, -y);
-        _boardTiles[x, y] = tile;
+      GameObject tilesObs = new GameObject();
+      tilesObs.name = "tilesObs";
+      tilesObs.transform.SetParent(this.transform);
+
+      GameObject[] lastLine = new GameObject[_boardSize.x];
+      GameObject lastTile = null;
+
+      for (int y = 0; y < _boardSize.y; y++)
+      {
+        for (int x = 0; x < _boardSize.x; x++)
+        {
+          List<GameObject> possibleTile = new List<GameObject>();
+          possibleTile.AddRange(_tilesPrefabs);
+
+          possibleTile.Remove(lastLine[x]);
+          possibleTile.Remove(lastTile);
+
+          int randomTile = Random.Range(0, possibleTile.Count);
+          GameObject tile = Instantiate(possibleTile[randomTile], tilesObs.transform);
+          tile.name = $"Tile {x}-{y}";
+          tile.transform.position = new Vector2(x, -y);
+          _boardTiles[x, y] = tile;
+
+          lastLine[x] = possibleTile[randomTile];
+          lastTile = possibleTile[randomTile];
+        }
       }
     }
   }
--- Assets/Scripts/Enum/TileTypes.cs
+++ Assets/Scripts/Enum/TileTypes.cs
@@ -1,7 +1,8 @@
-﻿public enum TileTypes {
-    Blue = 0,
-    Gray = 1,
-    Green = 2,
-    Red = 3,
-    Yellow = 4
+﻿public enum TileTypes
+{
+  Blue = 0,
+  Gray = 1,
+  Green = 2,
+  Red = 3,
+  Yellow = 4
 }
