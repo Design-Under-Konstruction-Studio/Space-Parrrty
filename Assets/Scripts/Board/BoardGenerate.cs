5f943c8caed0ef6925ca1d62666c1f0b3e858496
Jordan Oliveira <jordandossantosoliveira@gmail.com>
2020-07-01 20:57:29 -0300

    fix: Generate tiles with reference to the position of the Board object

--- Assets/Scripts/Board/BoardGenerate.cs
+++ Assets/Scripts/Board/BoardGenerate.cs
@@ -16,14 +16,15 @@ namespace Board
 
     [Header("Board Settings")]
     public Vector2Int startTileSize;
-    
+
     private void Awake()
     {
       boardController = GetComponent<BoardController>();
     }
 
-    private void Start() {
-        CreateBoard();
+    private void Start()
+    {
+      CreateBoard();
     }
 
     public void CreateBoard()
@@ -34,6 +35,7 @@ namespace Board
       boardController.tilesObjects = new GameObject();
       boardController.tilesObjects.name = "tilesObjects";
       boardController.tilesObjects.transform.SetParent(boardController.boardTransform.transform);
+      boardController.tilesObjects.transform.localPosition = Vector3.zero;
 
       GameObject[] lastLine = new GameObject[boardController.boardSize.x];
       GameObject lastTile = null;
@@ -62,9 +64,11 @@ namespace Board
 
       boardController.bottomOfBoard = boardController.boardSize.y;
     }
-  
-    public void createUpLine() {
-      for (int x = 0; x < boardController.boardSize.x; x++) {
+
+    public void createUpLine()
+    {
+      for (int x = 0; x < boardController.boardSize.x; x++)
+      {
         List<GameObject> possibleTile = new List<GameObject>();
 
         possibleTile.AddRange(_tilesPrefabs);
@@ -75,12 +79,14 @@ namespace Board
         boardController.boardTiles[x, boardController.bottomOfBoardOffSet] = tile;
       }
     }
-    
-    public void createDownLine() {
-      List<GameObject> lastLine = boardController.getTileLineGameObject(boardController.bottomOfBoardOffSet-1);
+
+    public void createDownLine()
+    {
+      List<GameObject> lastLine = boardController.getTileLineGameObject(boardController.bottomOfBoardOffSet - 1);
       GameObject lastTile = null;
-      
-      for (int x = 0; x < boardController.boardSize.x; x++) {
+
+      for (int x = 0; x < boardController.boardSize.x; x++)
+      {
         List<GameObject> possibleTile = new List<GameObject>();
 
         possibleTile.AddRange(_tilesPrefabs);
@@ -100,6 +106,6 @@ namespace Board
       boardController.bottomOfBoard++;
       boardController.topOfBoard++;
     }
-  
+
   }
 }
