fac73d9421e44ae69ed8ca0d21ba9b2e50462570
Jordan Oliveira <jordandossantosoliveira@gmail.com>
2020-12-03 22:04:19 -0300

    Criação do Script responsavel pelo obstaculo

---
+++ Assets/Scripts/Board/Elements/Obstacle/Obstacle.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+using Board.Elements.Base;
+
+namespace Board.Elements.Obstacle {
+    public class Obstacle : BoardElement
+    {
+        override protected void setElementName() {
+
+        }
+        override protected void updateCurrentPosition() {
+
+        }
+        override protected void onPositionSet() {
+
+        }
+        override public void onPickerActivated(Vector2Int selectedNeighbourPosition) {
+
+        }
+        
+        override public void onDestructionBelow() {
+
+        }
+        override public void fall() {
+
+        }
+        
+        override protected void onBeforeDestroy() {
+
+        }
+        override protected void showDestructionAppearance() {
+
+        }
+        override protected void onDestroy() {
+
+        }
+        override protected void onAfterDestroy() {
+
+        }
+        
+    }
+}
