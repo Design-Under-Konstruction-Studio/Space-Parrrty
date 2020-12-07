44dc600526dff6fd6d91bee3f2c20dbc1f2a2041
Jordan Oliveira <jordandossantosoliveira@gmail.com>
2020-12-07 19:51:41 -0300

    Criação obstaculo: Tamanho customizavel/Posição aleatoria #26

--- Assets/Scripts/Board/Behaviour/BoardGenerate.cs
+++ Assets/Scripts/Board/Behaviour/BoardGenerate.cs
@@ -95,17 +95,18 @@ namespace Board.Behaviour
             }
         }
 
-        public void createObstacle()
-        {
-            GameObject obstacleGO = Instantiate(_obstaclePrefab, new Vector3(100, 100, 100), Quaternion.identity, boardController.tilesObjects.transform);
-            Obstacle obstacle = obstacleGO.GetComponent<Obstacle>();
+        public void createObstacle(int level = 3)
+        {       
+            int randomPosition = Random.Range(0, boardController.boardSize.x - (level-1));
+            for(int i = 0; i<level;i++) {
+                GameObject obstacleGO = Instantiate(_obstaclePrefab, new Vector3(100, 100, 100), Quaternion.identity, boardController.tilesObjects.transform);
+                Obstacle obstacle = obstacleGO.GetComponent<Obstacle>();
 
-            Vector2 obstacleScale = obstacleGO.transform.localScale;
-            // obstacleScale.x *= obstacle.ObstacleSize;
-            obstacleGO.transform.localScale = obstacleScale;
-
-            obstacle.Position = new Vector2Int(0, boardController.topOfBoard);
-            obstacle.fall();
+                Vector2 obstacleScale = obstacleGO.transform.localScale;
+                obstacleGO.transform.localScale = obstacleScale;
+                obstacle.Position = new Vector2Int(randomPosition + i, boardController.topOfBoard);
+                obstacle.fall();
+            }
         }
 
         public void createDownLine()
